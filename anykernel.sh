003326660a6a914a3c45de4206c61b4847f1bb4e
Harsh Shandilya <msfjarvis@gmail.com>
2019-08-26 00:59:38 +0530

    Caesium it is
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Illusion Kernel by akhilnarang
+kernel.string=Caesium Kernel by msfjarvis
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
